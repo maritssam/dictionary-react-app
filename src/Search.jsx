81bf045f76184901c162125c9b96d86ba33fada3
Ilenia <83571246+ilenia-n@users.noreply.github.com>
2022-09-22 17:19:12 +0100

    Added API Integration with dictionaryapi.dev
    - added basic API integration with https://dictionaryapi.dev
    - added axios npm and imported on the Search component

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,26 +1,34 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-function Search (){
-    const [keyword, setKeyword] = useState(null);
 
-    function searchWord(event) {
-        event.preventDefault();
-        alert(`Searching for ${keyword}..`);
-    }
+function Search() {
+  const [keyword, setKeyword] = useState(null);
 
-    function captureKeyword(event){
-        console.log(event.target.value)
-        setKeyword(event.target.value);
-    }
+  function getDefinition(response) {
+    console.log(response.data[0]);
+    alert(response.data[0].meanings[0].definitions[0].definition)
+  }
 
-    return (
-        <div className="Search">
-            <form onSubmit={searchWord}>
-                <input type="search" placeholder="search for a word..." onChange={captureKeyword}/>
-                <input type="submit" value="Search"/>
-            </form>
-        </div>
-    )
+  function searchWord(event) {
+    event.preventDefault();
+    let apiURl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios(apiURl).then(getDefinition)
+  }
+
+  function captureKeyword(event) {
+    console.log(event.target.value)
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Search">
+      <form onSubmit={searchWord}>
+        <input type="search" placeholder="search for a word..." onChange={captureKeyword} />
+        <input type="submit" value="Search" />
+      </form>
+    </div>
+  )
 }
 
 export default Search;
