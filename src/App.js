120d6079d7f41ab650e2f621118bee66c3e33031
Ilenia <83571246+ilenia-n@users.noreply.github.com>
2022-09-23 16:10:50 +0100

    Added heading title

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="container container-box">
+        <h1 className="mb-5">Dictionary</h1>
         <Search />
         <footer>
           <a href="https://github.com/ilenia-n/dictionary-react-app"> Open-source code</a> by Ilenia
