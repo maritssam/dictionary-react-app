bdeed7a134741110ac23ce4e3d7f7ef5e65ab777
Ilenia <83571246+ilenia-n@users.noreply.github.com>
2022-09-30 11:12:31 +0100

    Updated CSS width

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
   return (
     <div className="App">
       <div className="container container-box">
+        <div className="container">
         <img className="logo img-fluid" src={Logo} alt="logo" title="Dictionary logo"></img>
+        </div>
         <Search />
         <footer>
           <a href="https://github.com/ilenia-n/dictionary-react-app" target="_blank" rel="noopener noreferrer"> Open-source code</a> by Ilenia
